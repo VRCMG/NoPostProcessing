528432959b8dfa70c0a5fdd7dd6a2477e17ff56f
Plague <36628963+OFWModz@users.noreply.github.com>
2020-08-12 20:30:49 +0100

    Project Files

---
+++ NoPostProcessing/NoPostProcessing.cs
@@ -0,0 +1,94 @@
+﻿using MelonLoader;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace NoPostProcessing
+{
+    public class NoPostProcessing : MelonMod
+    {
+        #region Data Types
+            public static bool DisablePostProcessing = true;
+
+            public static bool WorldWasChanged = false;
+
+            public static float Delay = 0f;
+        #endregion
+
+        public override void OnApplicationStart()
+        {
+            //Register Config Category And Toggle Boolean If Not Present
+            MelonPrefs.RegisterCategory("NoPostProcessing", "NoPostProcessing");
+            MelonPrefs.RegisterBool("NoPostProcessing", "DisablePostProcessing", true, "Disable Post Processing");
+
+            //Update Local Boolean: DisablePostProcessing To The State Of The Boolean In Config So We Don't Need To Keep Pulling It From Config, Which Would Cause Unnecessary Disk Reads
+            DisablePostProcessing = MelonPrefs.GetBool("NoPostProcessing", "DisablePostProcessing");
+        }
+
+        public override void OnModSettingsApplied()
+        {
+            //Assume This Mod's Config Boolean Was Modified, Even If It Was Not & Act On That Assumption
+
+            //Update Local Boolean: DisablePostProcessing To The State Of The Boolean In Config So We Don't Need To Keep Pulling It From Config, Which Would Cause Unnecessary Disk Reads
+            DisablePostProcessing = MelonPrefs.GetBool("NoPostProcessing", "DisablePostProcessing");
+
+            //Enumerate All Cameras
+            foreach (Camera cam in Camera.allCameras)
+            {
+                //If The Camera Has Post Processing Applied To It
+                if (cam.GetComponent<PostProcessLayer>() != null)
+                {
+                    //If The Camera's Post Processing Layer Has Not Been Toggled Previously
+                    if (!DisablePostProcessing != cam.GetComponent<PostProcessLayer>().enabled)
+                    {
+                        //Variably Log If The Post Processing Layer Was Removed Or Added
+                        MelonLogger.Log(DisablePostProcessing ? "Removed Post Processing From World!" : "Re-Added Post Processing From World!");
+
+                        //Set The Post Processing Layer On The Camera To The Opposite State Of DisablePostProcessing
+                        cam.GetComponent<PostProcessLayer>().enabled = !DisablePostProcessing;
+                    }
+                }
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            //Do Not Run Any Code Below Unless The Delay Has Passed & The World Has Changed
+            if (WorldWasChanged && Delay < Time.time)
+            {
+                //Define WorldWasChanged As False Until Next World Change
+                WorldWasChanged = false;
+
+                //Enumerate All Cameras
+                foreach (Camera cam in Camera.allCameras)
+                {
+                    //If The Camera Has Post Processing Applied To It
+                    if (cam.GetComponent<PostProcessLayer>() != null)
+                    {
+                        //If The Camera's Post Processing Layer Has Not Been Toggled Previously
+                        if (!DisablePostProcessing != cam.GetComponent<PostProcessLayer>().enabled)
+                        {
+                            //Variably Log If The Post Processing Layer Was Removed Or Added
+                            MelonLogger.Log(DisablePostProcessing ? "Removed Post Processing From World!" : "Re-Added Post Processing From World!");
+
+                            //Set The Post Processing Layer On The Camera To The Opposite State Of DisablePostProcessing
+                            cam.GetComponent<PostProcessLayer>().enabled = !DisablePostProcessing;
+                        }
+                    }
+                }
+            }
+        }
+
+        public override void OnLevelWasInitialized(int level)
+        {
+            //If World Was Initialized (Not Login/Loading)
+            if (level == -1)
+            {
+                //Define Boolean Stating The World Was Changed
+                WorldWasChanged = true;
+
+                //Define Delay Offset To Use In OnUpdate()
+                Delay = Time.time + 5f;
+            }
+        }
+    }
+}
---
+++ NoPostProcessing/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NoPostProcessing")]
+[assembly: AssemblyDescription("Permenantly Removes Post Processing From VRChat.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NoPostProcessing")]
+[assembly: AssemblyCopyright("Copyright © Plague 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: MelonInfo(typeof(NoPostProcessing.NoPostProcessing), "NoPostProcessing", null, "Plague", null)]
+[assembly: MelonGame(null, null)]
+[assembly: MelonOptionalDependencies("UIExpansionKit")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2da607c5-dc5c-48ee-b603-b98a81d21f08")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
